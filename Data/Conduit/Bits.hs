220ee49bfe1f66b595b3f5d86125c2f77535a488
Hideyuki Tanaka <tanaka.hideyuki@gmail.com>
2012-06-14 23:03:27 +0900

    add document

--- Data/Conduit/Bits.hs
+++ Data/Conduit/Bits.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE BangPatterns #-}
 
 module Data.Conduit.Bits (
+  -- * Conduits
   decodeBits,
   encodeBits,
   ) where
@@ -12,6 +13,7 @@ import Data.Conduit
 import qualified Data.Conduit.Binary as CB
 import Data.Conduit.Internal
 
+-- | Bitstream decoder
 decodeBits :: Monad m => Conduit S.ByteString m Bool
 decodeBits = go where
   go = do
@@ -23,6 +25,7 @@ decodeBits = go where
         forM_ [0..7] $ \i -> yield $ testBit b8 i
         go
 
+-- | Bitstream encoder
 encodeBits :: Monad m => Conduit Bool m S.ByteString
 encodeBits = go 0 0 where
   go !acc 8 = do
